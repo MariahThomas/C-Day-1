527e015ee938c4fe9528c2c194d761b3ca641482
Mariah Thomas <mariahthomasmampilly@gmail.com>
2021-04-22 11:51:22 +0530

    first commit

---
+++ File1.cs
@@ -0,0 +1,16 @@
+using System.IO;
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int a=6;
+       int b=8;
+       Console.WriteLine(a+b);
+       Console.WriteLine(a-b);
+       Console.WriteLine(a*b);
+       Console.WriteLine(a/b);
+       Console.ReadLine();
+    }
+}
